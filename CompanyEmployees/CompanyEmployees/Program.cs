2929f6fdf04d570a040ecd8894a6aff252927806
Borushko Dima <borushkoddima@mail.ru>
2024-02-19 19:26:13 +0300

    Добавили файл для логгера nlog.config на основном проекте и его реализацию

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -1,5 +1,6 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 namespace CompanyEmployees
 {
@@ -9,8 +10,11 @@ namespace CompanyEmployees
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
             builder.Services.ConfigureCors();
             builder.Services.ConfigureIISIntegration();
+            builder.Services.ConfigureLoggerService();
 
             builder.Services.AddControllers();
 
