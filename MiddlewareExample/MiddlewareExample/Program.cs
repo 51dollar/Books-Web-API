d117b149b94886f68ea78b3d30629e0927293e5d
Borushko Dima <borushkoddima@mail.ru>
2024-02-19 15:24:29 +0300

    Работа с методом Use

--- MiddlewareExample/MiddlewareExample/Program.cs
+++ MiddlewareExample/MiddlewareExample/Program.cs
@@ -18,9 +18,17 @@ namespace MiddlewareExample
 
             app.UseAuthorization();
 
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine($"Logic before executing the next delegate in the Use method");
+                await next.Invoke();
+                Console.WriteLine($"Logic after executing the next delegate in the Use method");
+            });
+
             app.Run(async context =>
             {
-                await context.Response.WriteAsync("Привет это middleware компонент.");
+                Console.WriteLine($"Writing the response to the client in the Run method");
+                await context.Response.WriteAsync("Hello from the middleware component.");
             });
 
             app.MapControllers();
