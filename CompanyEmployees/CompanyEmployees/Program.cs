25cdc98372fa3b0f4356a978b214294cdaaf211a
Borushko Dima <borushkoddima@mail.ru>
2024-02-24 12:49:27 +0300

    fix nlog

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -10,7 +10,7 @@ namespace CompanyEmployees
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+            LogManager.Setup().LoadConfigurationFromFile("/nlog.config");
 
             builder.Services.ConfigureCors();
             builder.Services.ConfigureIISIntegration();
