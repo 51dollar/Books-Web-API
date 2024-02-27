ec2f0442d9b4ae1ae7d95c596aeae852f39b48b9
Borushko Dima <borushkoddima@mail.ru>
2024-02-27 10:53:00 +0300

    add RepositoryBase in him Create, Update, Delete and optimization request

---
+++ Repository/Repository/RepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext) =>
+            RepositoryContext = repositoryContext;
+
+        //Параметр trackChanges нужен для повышения производительности запросов только для чтения.
+        //Если для него установлено значение false, мы присоединяем к нашему запросу метод AsNoTracking,
+        //чтобы сообщить EF Core о том, что ему не нужно отслеживать изменения для необходимых сущностей.
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+                RepositoryContext.Set<T>()
+                    .AsNoTracking() :
+                RepositoryContext.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ?
+                RepositoryContext.Set<T>()
+                    .Where(expression)
+                    .AsNoTracking() :
+                RepositoryContext.Set<T>()
+                    .Where(expression);
+
+        public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
+        public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
+        public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
+    }
+}
