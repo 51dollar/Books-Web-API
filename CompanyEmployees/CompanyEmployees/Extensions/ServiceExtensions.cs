f8a3ac4c6d9fe68ded9bb4f25a8e4d72e8e8bc20
Borushko Dima <borushkoddima@mail.ru>
2024-03-06 20:20:20 +0300

    fix Test Postman 200 OK

--- CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -32,6 +32,6 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureSqlContext(this IServiceCollection services,
             IConfiguration configuration) =>
             services.AddDbContext<RepositoryContext>(opts =>
-                opts.UseSqlServer(configuration.GetConnectionString("sqlConnecktion")));
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
     }
 }
