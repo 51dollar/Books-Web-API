2d848bc2471b348cfb3de146f0c0b50e2176eb3a
Borushko Dima <borushkoddima@mail.ru>
2024-03-11 13:00:47 +0300

    add AutoMapper

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -18,6 +18,7 @@ namespace CompanyEmployees
             builder.Services.ConfigureRepositoryManager();
             builder.Services.ConfigureServiceManager();
             builder.Services.ConfigureSqlContext(builder.Configuration);
+            builder.Services.AddAutoMapper(typeof(Program));
 
             builder.Services.AddControllers()
                 .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
