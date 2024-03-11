d22360c33da48177c0226c1925f85fcbafd130be
Borushko Dima <borushkoddima@mail.ru>
2024-03-11 11:39:47 +0300

    add DTO and its connection

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -1,9 +1,9 @@
-﻿using Entities;
+﻿using Shared.DataTransferObjects;
 
 namespace Service
 {
     public interface ICompanyService
     {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     }
 }
