171f7166bba52696d07c3c17088b8866c5a2e617
Borushko Dima <borushkoddima@mail.ru>
2024-03-14 17:24:12 +0300

    add castom Exceptions and check for correct id

--- CompanyEmployees/Contracts/ICompanyRepository.cs
+++ CompanyEmployees/Contracts/ICompanyRepository.cs
@@ -5,5 +5,6 @@ namespace Repository
     public interface ICompanyRepository
     {
         IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        Company GetCompany(Guid companyId, bool trackChanges);
     }
 }
