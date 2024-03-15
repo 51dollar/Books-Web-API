04ced4f4271b08261b950eed155fb93774b8a568
Borushko Dima <borushkoddima@mail.ru>
2024-03-15 14:39:48 +0300

    add POST CreateCompany also, add status code 201 (CreatedAtRoute)

--- CompanyEmployees/Contracts/ICompanyRepository.cs
+++ CompanyEmployees/Contracts/ICompanyRepository.cs
@@ -6,5 +6,6 @@ namespace Repository
     {
         IEnumerable<Company> GetAllCompanies(bool trackChanges);
         Company GetCompany(Guid companyId, bool trackChanges);
+        void CreateCompany(Company company);
     }
 }
--- CompanyEmployees/Service.Contracts/ICompanyService.cs
+++ CompanyEmployees/Service.Contracts/ICompanyService.cs
@@ -6,5 +6,6 @@ namespace Service
     {
         IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
         CompanyDto GetCompany(Guid companyId, bool trackChanges);
+        CompanyDto CreateCompany(CompanyForCreationDto company);
     }
 }
