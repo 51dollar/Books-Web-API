f59121851517a5f1e8764e2bd9ee47840860513f
Borushko Dima <borushkoddima@mail.ru>
2024-03-22 15:05:16 +0300

    add DeleteEmployeeForCompany

--- CompanyEmployees/Repository/EmployeeRepository.cs
+++ CompanyEmployees/Repository/EmployeeRepository.cs
@@ -21,5 +21,8 @@ namespace Repository
             employee.CompanyId = companyId;
             Create(employee);
         }
+
+        public void DeleteEmployee(Employee employee) =>
+            Delete(employee);
     }
 }
