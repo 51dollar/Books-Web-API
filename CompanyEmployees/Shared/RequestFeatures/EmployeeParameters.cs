0fb5501a7469a510bf60323dde4f73bec49d34b5
Borushko Dima <borushkoddima@mail.ru>
2024-04-07 17:19:08 +0300

    Filtering
    https://localhost:5001/api/companies/C9D4C053-49B6-410C-BC78- 2D54A9991870/employees?minAge=26&maxAge=30

--- CompanyEmployees/Shared/RequestFeatures/EmployeeParameters.cs
+++ CompanyEmployees/Shared/RequestFeatures/EmployeeParameters.cs
@@ -1,8 +1,10 @@
-﻿using Shared.RequestFeatures;
-
-namespace Shared.RequestFeatures
+﻿namespace Shared.RequestFeatures
 {
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = int.MaxValue;
+
+        public bool ValidAgeRange => MaxAge > MinAge;
     }
 }
