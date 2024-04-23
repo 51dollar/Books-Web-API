cd3a888f0d925c9ad95876fd286f6b43b451f3bb
Borushko Dima <borushkoddima@mail.ru>
2024-04-23 17:34:58 +0300

    add migration change

--- CompanyEmployees/CompanyEmployees/Migrations/20240423142525_AdditionalUserFiledsForRefreshToken.Designer.cs
+++ CompanyEmployees/CompanyEmployees/Migrations/20240423142525_AdditionalUserFiledsForRefreshToken.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -11,9 +12,11 @@ using Repository;
 namespace CompanyEmployees.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240423142525_AdditionalUserFiledsForRefreshToken")]
+    partial class AdditionalUserFiledsForRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -227,13 +230,13 @@ namespace CompanyEmployees.Migrations
                     b.HasData(
                         new
                         {
-                            Id = "4ac8240a-8498-4869-bc86-60e5dc982d27",
+                            Id = "373eeee2-dade-4943-a2f6-a0ac6ca5efd6",
                             Name = "Manager",
                             NormalizedName = "MANAGER"
                         },
                         new
                         {
-                            Id = "562419f5-eed1-473b-bcc1-9f2dbab182b4",
+                            Id = "b017e922-c173-457e-a807-bc1e676d22f7",
                             Name = "Administrator",
                             NormalizedName = "ADMINISTRATOR"
                         });
